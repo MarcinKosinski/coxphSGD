2b8ce13acf20877d8146d6b723d2a3891ffa8149
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-11-16 23:17:26 +0100

    Symulacje w rozdziale 4

--- Master_Thesis/dataCox.R
+++ Master_Thesis/dataCox.R
@@ -19,76 +19,81 @@ x <- matrix(sample(0:1, size = 20000, replace = TRUE), ncol = 2)
 
 dataCox(10^4, lambda = 3, rho = 2, x, beta = c(1,3), censRate = 5) -> dCox
 
-# podziel na pozdzbiory
-sample(1:90, size = 10^4, replace = TRUE) -> group
 
-split(dCox, group) -> dCox_splitted
 
-#table(unlist(lapply(dCox_splitted, nrow)))
-
-table(group) %>% as.matrix
 
 library(coxphSGD)
-
-
-coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
-         learningRates = function(x){1/x}) -> estimates
-
-
-
-sample(1:60, size = 10^4, replace = TRUE) -> group
-
-split(dCox, group) -> dCox_splitted
-
-#table(unlist(lapply(dCox_splitted, nrow)))
-
-table(group) %>% as.matrix
-
-library(coxphSGD)
-
-
-coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
-         learningRates = function(x){1/x}) -> estimates2
-
-
-
-sample(1:120, size = 10^4, replace = TRUE) -> group
-
-split(dCox, group) -> dCox_splitted
-
-#table(unlist(lapply(dCox_splitted, nrow)))
-
-table(group) %>% as.matrix
-
-library(coxphSGD)
-
-
-coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
-         learningRates = function(x){1/x}) -> estimates3
-
-
-
-
-summary(coxph(Surv(time, status)~x.1+x.2, data = dCox))
-
 library(ggplot2)
-t(simplify2array(estimates$coefficients)) %>%
-  as.data.frame() -> df1
-t(simplify2array(estimates2$coefficients)) %>%
-  as.data.frame() -> df2
-t(simplify2array(estimates3$coefficients)) %>%
-  as.data.frame() -> df3
 library(dplyr)
-df1 %>%
-  mutate(version = "90 batches") %>%
-  bind_rows(df2 %>%
-              mutate(version = "60 batches")) %>%
-  bind_rows(df3 %>%
-              mutate(version = "120 batches")) %>%
-  ggplot(aes(x.1, x.2, group = version, colour = version)) +
-  geom_path() +
-  theme_bw(base_size = 20) +
-  theme(panel.border = element_blank(),
-        legend.key = element_blank()) +
-  scale_colour_brewer(palette="Dark2", name = 'Algorithm') +
-  coord_cartesian( ylim = c(-10,5))
+
+vizCoxSGD <- function(dCox = dCox){
+
+  # podziel na pozdzbiory
+  sample(1:90, size = 10^4, replace = TRUE) -> group
+
+  split(dCox, group) -> dCox_splitted
+  coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
+           learningRates = function(x){1/x}) -> estimates
+
+
+
+  sample(1:60, size = 10^4, replace = TRUE) -> group
+
+  split(dCox, group) -> dCox_splitted
+
+
+  coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
+           learningRates = function(x){1/x}) -> estimates2
+
+
+
+  sample(1:120, size = 10^4, replace = TRUE) -> group
+
+  split(dCox, group) -> dCox_splitted
+
+  coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = 1e-03,
+           learningRates = function(x){1/x}) -> estimates3
+
+
+
+  t(simplify2array(estimates$coefficients)) %>%
+    as.data.frame() -> df1
+  t(simplify2array(estimates2$coefficients)) %>%
+    as.data.frame() -> df2
+  t(simplify2array(estimates3$coefficients)) %>%
+    as.data.frame() -> df3
+
+  df1 %>%
+    mutate(version = "90 batches") %>%
+    bind_rows(df2 %>%
+                mutate(version = "60 batches")) %>%
+    bind_rows(df3 %>%
+                mutate(version = "120 batches")) %>%
+    ggplot(aes(x.1, x.2, group = version, colour = version)) +
+    geom_path() +
+    theme_bw(base_size = 20) +
+    theme(panel.border = element_blank(),
+          legend.key = element_blank()) +
+    scale_colour_brewer(palette="Dark2", name = 'Algorithm') +
+    coord_cartesian( ylim = c(-10,5)) -> plotX
+
+  return(plotX)
+}
+
+vizCoxSGD(dCox) -> plot1
+vizCoxSGD(dCox) -> plot2
+vizCoxSGD(dCox) -> plot3
+vizCoxSGD(dCox) -> plot4
+
+pdf(file = "plot1_cox.pdf", width = 10, height =8)
+plot(plot1)
+dev.off()
+pdf(file = "plot2_cox.pdf", width = 10, height =8)
+plot(plot2)
+dev.off()
+pdf(file = "plot3_cox.pdf", width = 10, height =8)
+plot(plot3)
+dev.off()
+pdf(file = "plot4_cox.pdf", width = 10, height =8)
+plot(plot4)
+dev.off()
