9201be834cf07e96ba3e5084c8ac075d0e5a5218
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-12-02 02:02:22 +0100

    Dodanie epok, warstwic i usprawienie wykresow

--- Master_Thesis/dataCox.R
+++ Master_Thesis/dataCox.R
@@ -1,3 +1,4 @@
+set.seed(456)
 dataCox <- function(N, lambda, rho, x, beta, censRate){
 
   # real Weibull times
@@ -19,22 +20,61 @@ x <- matrix(sample(0:1, size = 20000, replace = TRUE), ncol = 2)
 
 dataCox(10^4, lambda = 3, rho = 2, x, beta = c(1,3), censRate = 5) -> dCox
 
+library(dplyr)
+dCox %>%
+  dplyr::arrange(time) -> dCoxArr
+
+# assuming observations are sorted by time
+full_cox_loglik <- function(beta1, beta2, x1, x2, censored){
+  sum(rev(censored)*(beta1*rev(x1) + beta2*rev(x2) -
+                       log(cumsum(exp(beta1*rev(x1) + beta2*rev(x2))))))
+}
+
+library(reshape2)
+calculate_outer_cox <- function(x1, x2, censored){
+  ## contours
+  outer_res <- outer(seq(-1,3, length = 100),
+                     seq(0,4, length = 100),
+                     Vectorize( function(beta1,beta2){
+                       full_cox_loglik(beta1, beta2, x1 = x1, x2 = x2, censored = censored)
+                     } )
+  )
+  outer_res_melted <- melt(outer_res)
+  outer_res_melted$Var1 <- as.factor(outer_res_melted$Var1)
+  levels(outer_res_melted$Var1) <- as.character(seq(-1,3, length = 100))
+  outer_res_melted$Var2 <- as.factor(outer_res_melted$Var2)
+  levels(outer_res_melted$Var2) <- as.character(seq(0,4, length = 100))
+  outer_res_melted$Var1 <- as.numeric(as.character(outer_res_melted$Var1))
+  outer_res_melted$Var2 <- as.numeric(as.character(outer_res_melted$Var2))
+  return(outer_res_melted)
+}
 
 
+calculate_outer_cox(dCoxArr$x.1, dCoxArr$x.2, dCoxArr$status) -> outerCox
 
-library(coxphSGD)
+
 library(ggplot2)
+ggplot()+
+  #   stat_contour(aes(x=outerCox$Var1,
+  #                    y=outerCox$Var2,
+  #                    z=outerCox$value),
+  #                binwidth = 2, size = 0.5, colour = "grey50") +
+  stat_contour(aes(x=outerCox$Var1,
+                   y=outerCox$Var2,
+                   z=outerCox$value),
+               bins = 40)
+library(coxphSGD)
 library(dplyr)
 
-vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
-                      epsilon = 1e-03, beta_0 = c(0,0), solution = c(1,3)){
+simulateCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
+                      epsilon = 1e-03, beta_0 = c(0,0), max.iter = 100){
 
   # podziel na pozdzbiory
   sample(1:90, size = 10^4, replace = TRUE) -> group
 
   split(dCox, group) -> dCox_splitted
   coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
-           learningRates = learningRates, beta_0 = beta_0) -> estimates
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*90) -> estimates
 
 
 
@@ -44,7 +84,7 @@ vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
 
 
   coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
-           learningRates = learningRates, beta_0 = beta_0) -> estimates2
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*60) -> estimates2
 
 
 
@@ -53,7 +93,7 @@ vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
   split(dCox, group) -> dCox_splitted
 
   coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
-           learningRates = learningRates, beta_0 = beta_0) -> estimates3
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*120) -> estimates3
 
 
   sample(1:200, size = 10^4, replace = TRUE) -> group
@@ -61,7 +101,7 @@ vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
   split(dCox, group) -> dCox_splitted
 
   coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
-           learningRates = learningRates, beta_0 = beta_0) -> estimates4
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*200) -> estimates4
 
 
   sample(1:30, size = 10^4, replace = TRUE) -> group
@@ -69,7 +109,15 @@ vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
   split(dCox, group) -> dCox_splitted
 
   coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
-           learningRates = learningRates, beta_0 = beta_0) -> estimates5
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*30) -> estimates5
+
+
+  sample(1:10, size = 10^4, replace = TRUE) -> group
+
+  split(dCox, group) -> dCox_splitted
+
+  coxphSGD(Surv(time, status)~x.1+x.2, data = dCox_splitted, epsilon = epsilon,
+           learningRates = learningRates, beta_0 = beta_0, max.iter = max.iter*10) -> estimates6
 
 
 
@@ -84,31 +132,55 @@ vizCoxSGD <- function(dCox = dCox, learningRates = function(x){1/x},
     as.data.frame() -> df4
   t(simplify2array(estimates5$coefficients)) %>%
     as.data.frame() -> df5
+  t(simplify2array(estimates6$coefficients)) %>%
+    as.data.frame() -> df6
 
   df1 %>%
-    mutate(version = "90 batches") %>%
+    mutate(version = paste("90 batches,", nrow(df1), " steps")) %>%
     bind_rows(df2 %>%
-                mutate(version = "60 batches")) %>%
+                mutate(version = paste("60 batches,", nrow(df2), " steps"))) %>%
     bind_rows(df3 %>%
-                mutate(version = "120 batches")) %>%
+                mutate(version = paste("120 batches,", nrow(df3), " steps"))) %>%
     bind_rows(df4 %>%
-                mutate(version = "200 batches")) %>%
+                mutate(version = paste("200 batches,", nrow(df4), " steps"))) %>%
     bind_rows(df5 %>%
-              mutate(version = "30 batches")) %>%
-    ggplot(aes(V1, V2, group = version, colour = version)) +
-    geom_point(x = solution[1], y = solution[2], col = "black", size = 4, shape = 15) +
-    geom_path() +
-    theme_bw(base_size = 20) +
-    theme(panel.border = element_blank(),
-          legend.key = element_blank()) +
-    scale_colour_brewer(palette="Set1", name = 'Algorithm') +
-     geom_point(x = beta_0[1], y = beta_0[2], col = "black", size = 4, shape = 17) +
-    xlab("X1") + ylab("X2") -> plotX
-    #coord_cartesian( ylim = c(-10,5)) -> plotX
+                mutate(version = paste("30 batches,", nrow(df5), " steps"))) %>%
+    bind_rows(df6 %>%
+                mutate(version = paste("10 batches,", nrow(df6), " steps"))) -> d2ggplot
 
-  return(list(plot = plotX, est1 = estimates, est2 = estimates2,
+  return(list(d2ggplot = d2ggplot, est1 = estimates, est2 = estimates2,
               est3 = estimates3, est4 = estimates4, est5 = estimates5))
+
 }
+
+
+
+simulateCoxSGD(dCox, learningRates = function(x){1/(100*sqrt(x))}, max.iter = 5) -> d2ggplot
+d2ggplot -> backpack
+d2ggplot <- d2ggplot$d2ggplot
+beta_0 = c(0,0)
+solution = c(1,3)
+
+ggplot() +
+  stat_contour(aes(x=outerCox$Var1,
+                   y=outerCox$Var2,
+                   z=outerCox$value),
+               bins = 40, alpha = 0.25) +
+  geom_path(aes(d2ggplot$V1, d2ggplot$V2, group = d2ggplot$version,
+                colour = d2ggplot$version), size = 1) +
+  theme_bw(base_size = 20) +
+  theme(panel.border = element_blank(),
+        legend.key = element_blank(), legend.position = "top") +
+  scale_colour_brewer(palette="Dark2", name = 'Algorithm \n & Steps') +
+  geom_point(aes(x = beta_0[1], y = beta_0[2]), col = "black", size = 4, shape = 17) +
+  geom_point(aes(x = solution[1], y = solution[2]), col = "black", size = 4, shape = 15) +
+  xlab("X1") + ylab("X2") +
+  guides(col = guide_legend(ncol = 3))
+
+
+
+
+vizCoxSGD(I2,  outerCox = outerCox) -> plot6
 
 vizCoxSGD(dCox) -> plot1
 vizCoxSGD(dCox, learningRates = function(x){1/sqrt(x)}) -> plot2
